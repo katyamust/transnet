5386222012d1a09ed2110d437bc85a357cdd68ef
Katya Mustafina <katya.mustafina@gmail.com>
2017-11-22 10:39:31 +0100

    latest

--- preprocess.py
+++ preprocess.py
@@ -1,24 +1,18 @@
 import pandas as pd
 import os
 
-#bdown_table_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/bdown_tbl.csv"
-#df_bdown_table = pd.read_csv(bdown_table_csv_fname, encoding="UTF-8")
+bdown_table_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/bdown_tbl.csv"
+df_bdown_table = pd.read_csv(bdown_table_csv_fname, encoding="UTF-8")
 
 equipmentmappings_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/equipmentmappings.csv"
 df_ioxnxt_equipment_mapping = pd.read_csv(equipmentmappings_csv_fname)
 
-#agent_history_straddle_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/assethistory_november.csv"
-#df_agent_history_straddle = pd.read_csv(agent_history_straddle_csv_fname)
-
-
-#print(df_bdown_table.shape)
-#print(df_bdown_table.head())
+print(df_bdown_table.shape)
+print(df_bdown_table.head())
 
 #print(df_ioxnxt_equipment_mapping.shape)
 #print(df_ioxnxt_equipment_mapping.head())
 
-#print(df_agent_history_straddle.shape)
-#print(df_agent_history_straddle.head())
 
 
 def replace_id_mapping ():
@@ -27,8 +21,29 @@ def replace_id_mapping ():
     df_ioxnxt_equipment_mapping["n_id"] = df_ioxnxt_equipment_mapping["s_id"].apply(lambda x: x[2:5])
 
     print(df_ioxnxt_equipment_mapping.columns.tolist())
-    print(df_ioxnxt_equipment_mapping["n_id"] )
+    print(df_ioxnxt_equipment_mapping[["s_id","n_id","key"]] )
+
+    dict = df_ioxnxt_equipment_mapping.set_index('key').to_dict()
+   # gby = df_ioxnxt_equipment_mapping.groupby("key").to_dict()
+    print(dict.keys)
 
 replace_id_mapping()
+
+def merge_tables():
+    agent_history_straddle_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/assethistory_november.csv"
+    df_agent_history_straddle = pd.read_csv(agent_history_straddle_csv_fname)
+
+    print(df_agent_history_straddle.shape)
+    print(df_agent_history_straddle.head())
+    print(df_agent_history_straddle["IGN.1.ON"]).unique()
+    #print(df_agent_history_straddle["KEY"].ix[1:10])
+
+    #create new id column
+    #df_agent_history_straddle["n_id"] = pd.Series(df_ioxnxt_equipment_mapping["n_id"].apply(lambda x: x[2:5]))
+    #df_agent_history_straddle["KEY"] = df_ioxnxt_equipment_mapping
+    #df_ioxnxt_equipment_mapping["s_id"]
+    return
+
+#merge_tables()
 
 print("Success")
