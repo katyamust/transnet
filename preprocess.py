7f9c09ce5535afc2a9ef887c832bf5fe3d047f40
jeroenterheerdt <jeroen_ter_heerdt@hotmail.com>
2017-11-22 15:10:42 +0200

    Added drop_columns() to drop columns flagged for ignore from the dataset.

--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import os
 
-bdown_table_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/bdown_tbl.csv"
+PATH_TO_DATA_FOLDER = r"C:\Users\jterh\Microsoft\OneDrive - Microsoft\_DX era\Hackathons\Machine Learning Hackfest South Africa\data"
+
+bdown_table_csv_fname = PATH_TO_DATA_FOLDER+r"\bdown_tbl.csv"
 df_bdown_table = pd.read_csv(bdown_table_csv_fname, encoding="UTF-8")
 
-equipmentmappings_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/equipmentmappings.csv"
+equipmentmappings_csv_fname = PATH_TO_DATA_FOLDER+r"\equipmentmappings.csv"
 df_ioxnxt_equipment_mapping = pd.read_csv(equipmentmappings_csv_fname)
 
 print(df_bdown_table.shape)
@@ -12,8 +14,6 @@ print(df_bdown_table.head())
 
 #print(df_ioxnxt_equipment_mapping.shape)
 #print(df_ioxnxt_equipment_mapping.head())
-
-
 
 def replace_id_mapping ():
     # replace equipmentmapping Straddle ID to new with SC222 -> 222
@@ -27,10 +27,10 @@ def replace_id_mapping ():
    # gby = df_ioxnxt_equipment_mapping.groupby("key").to_dict()
     print(dict.keys)
 
-replace_id_mapping()
+#replace_id_mapping()
 
 def merge_tables():
-    agent_history_straddle_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/assethistory_november.csv"
+    agent_history_straddle_csv_fname = PATH_TO_DATA_FOLDER+r"\assethistory_november.csv"
     df_agent_history_straddle = pd.read_csv(agent_history_straddle_csv_fname)
 
     print(df_agent_history_straddle.shape)
@@ -46,4 +46,17 @@ def merge_tables():
 
 #merge_tables()
 
+#after the merge, taking agenthistory_straddle_flat csv and dropping columns
+ahfile = PATH_TO_DATA_FOLDER+r'\agenthistory_straddle_flat.csv'
+df = pd.read_csv(ahfile, encoding='utf8')
+
+#preprocess: remove columns
+def drop_columns(df):
+    cols_to_remove = ['ID','VIB.1.MAX','GPS.1.GNDHEAD','GPS.1.ALT','GPS.1.LON','GPS.1.LAT','EntryTime15Mins','CAN.1.READY','PostUtc','FromUtc','GPS.1.SATS','GSM.1.RSSI','GEAR.1','ProcessUtc','GATEWAY.1.MAC','GATEWAY.1.VERSION','DIAG.1.THREAD','DIAG.1.EVENT','DIAG.1.MEM','ENGINE.1.OILLVL','NextBreakdown','Breakdown description','Reported by','Employee No','Description of repair','Shift','Status','Signed back by','Supervisor','OEM','Location']
+    df = df.drop(cols_to_remove,axis=1)
+    df.to_csv(PATH_TO_DATA_FOLDER+r'\agenthistory_straddle_flat_filtered.csv')
+    return df
+
+df = drop_columns(df)
+print(df.shape)
 print("Success")
