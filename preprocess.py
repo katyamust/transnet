7dabf87e7a281f0d78a81f69dc87d646c9091751
Katya Mustafina <katya.mustafina@gmail.com>
2017-11-22 09:10:58 +0100

    replace id mapping

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import os
 
-bdown_table_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/bdown_tbl.csv"
-df_bdown_table = pd.read_csv(bdown_table_csv_fname, encoding="UTF-8")
+#bdown_table_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/bdown_tbl.csv"
+#df_bdown_table = pd.read_csv(bdown_table_csv_fname, encoding="UTF-8")
 
 equipmentmappings_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/equipmentmappings.csv"
 df_ioxnxt_equipment_mapping = pd.read_csv(equipmentmappings_csv_fname)
 
-agent_history_straddle_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/assethistory_november.csv"
-df_agent_history_straddle = pd.read_csv(agent_history_straddle_csv_fname)
+#agent_history_straddle_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/assethistory_november.csv"
+#df_agent_history_straddle = pd.read_csv(agent_history_straddle_csv_fname)
 
 
 #print(df_bdown_table.shape)
@@ -17,9 +17,18 @@ df_agent_history_straddle = pd.read_csv(agent_history_straddle_csv_fname)
 #print(df_ioxnxt_equipment_mapping.shape)
 #print(df_ioxnxt_equipment_mapping.head())
 
-print(df_agent_history_straddle.shape)
-print(df_agent_history_straddle.head())
+#print(df_agent_history_straddle.shape)
+#print(df_agent_history_straddle.head())
 
 
+def replace_id_mapping ():
+    # replace equipmentmapping Straddle ID to new with SC222 -> 222
+    df_ioxnxt_equipment_mapping["n_id"] = pd.Series()
+    df_ioxnxt_equipment_mapping["n_id"] = df_ioxnxt_equipment_mapping["s_id"].apply(lambda x: x[2:5])
+
+    print(df_ioxnxt_equipment_mapping.columns.tolist())
+    print(df_ioxnxt_equipment_mapping["n_id"] )
+
+replace_id_mapping()
 
 print("Success")
