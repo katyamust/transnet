ebac178d41599fb7d00fa96575e360e0db6b79e2
Katya Mustafina <katya.mustafina@gmail.com>
2017-12-04 12:08:05 +0100

    config change, use json config file to profile path/locaiton info, added optional timeit

--- features.py
+++ features.py
@@ -1,18 +1,29 @@
 import pandas as pd
 from datetime import timedelta
-import datetime
+#import datetime
+import json
+import time
 
 #CONFIG
+
+with open('config.json') as json_config_file:
+    config = json.load(json_config_file)
+
 FEATURES_TO_INCLUDE = ["VIB.1.LIV","ENG.1.OILPRS","ENG.1.OILLVL","ENG.1.BAT","GPS.1.GNDSPD","SPRDR.1.SPD","FUEL.1.TNK2LVL","SPRDR.1.MOVEMENT","ENG.1.OILTEMP","BRAKE.1","RUNHRS.1"]
-PATH_TO_DATA_FOLDER = r"C:\Users\jterh\Microsoft\OneDrive - Microsoft\_DX era\Hackathons\Machine Learning Hackfest South Africa\data"
+PATH_TO_DATA_FOLDER = config["path"]["Katya"]
+TEST=1 # run on smaller data set for testing this code
 
-if 0:
-    agent_history_straddle_csv_fname = PATH_TO_DATA_FOLDER+r"\agenthistory_straddle_flat_filtered.csv"
+if TEST:
+    agent_history_straddle_csv_fname = PATH_TO_DATA_FOLDER + config["files"]["test_dataset"]
+    features_file = PATH_TO_DATA_FOLDER + config["files"]["test_featureset"]
 else:
-    agent_history_straddle_csv_fname = "C:/Users/kamustaf/Documents/transnet_data/agenthistory_straddle_flat_filtered.csv"
-    features_file = "C:/Users/kamustaf/Documents/transnet_data/features.csv"
+    agent_history_straddle_csv_fname = PATH_TO_DATA_FOLDER + config["files"]["dataset"]
+    features_file = PATH_TO_DATA_FOLDER + config["files"]["featureset"]
 
+start_time = time.time()
+print("loading dataframe ")
 df_agent_history_straddle = pd.read_csv(agent_history_straddle_csv_fname)
+print("--- %s seconds ---" % (time.time() - start_time))
 
 print(df_agent_history_straddle.shape)
 #print(df_agent_history_straddle.head())
@@ -55,19 +66,31 @@ def get_features_for_param(df,param_name, date,numdaysinhistory):
 #print(get_features_for_param(df_agent_history_straddle,'VIB.1.LIV', testdate,4))
 
 def get_features(df,date,numdaysinhistory):
-    print("get features")
-    
+    #print("get features for date")
+
+    #start_time = time.time()
+
     resultdf = pd.DataFrame()
     for f in FEATURES_TO_INCLUDE:
         r = get_features_for_param(df,f,date,numdaysinhistory)
         resultdf = pd.concat([resultdf,r],axis=1)
+
+    #print("--- %s seconds ---" % (time.time() - start_time))
+
     return resultdf
 
 
 def get_label(df, day):
+    #print("get labels")
+
+    #start_time= time.time()
+
     label = pd.DataFrame(data=[0],columns=["label"])
     if any(df[df.EntryDate == day].IncidentID.notna()):
         label = pd.DataFrame(data=[1],columns=["label"])
+
+    #print("--- %s seconds ---" % (time.time() - start_time))
+
     return label
 
 def get_straddle_features_alldates(df):
@@ -80,14 +103,17 @@ def get_straddle_features_alldates(df):
 
     for day in days:
         features_df = get_features(df,day,4)
-        print("features_df")
-        print(features_df.shape)
+        #print("features_df")
+        #print(features_df.shape)
         label_df = get_label(df,day)
 
         #concat with features_ df horisontally:
-        features_df = pd.concat([features_df,label_df],axis=1)
         print("features_df labels df concat")
+        start_time = time.time()
+        features_df = pd.concat([features_df,label_df],axis=1)
+
         print(features_df.shape)
+        print("--- %s seconds ---" % (time.time() - start_time))
 
         straddle_features = pd.concat([straddle_features,features_df],ignore_index=True)
 
@@ -121,9 +147,11 @@ def create_lagging_features(df):
 
     return df_all_features
 
+start_time = time.time()
 all_features = create_lagging_features(df_agent_history_straddle)
 print(all_features.shape)
 
 all_features.to_csv(features_file)
+print("--- %s seconds ---" % (time.time() - start_time))
 
 print("Done.")
